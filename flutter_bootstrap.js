714e0f3f0db9f401c35385f209bb92b6fa749073
sachinbrahmbhatt <barotsachin739@gmail.com>
2024-12-20 19:41:19 +0530

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3341523845"
+    serviceWorkerVersion: "1425185032"
   }
 });
